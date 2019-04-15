d256c05c6a41011058f9bed778e6d7ed9cd85f1b
danielminson <49560569+danielminson@users.noreply.github.com>
2019-04-15 10:13:37 -0300

    testando

--- ep1.py
+++ ep1.py
@@ -1,9 +1,11 @@
-# EP 2019-1: A vida no insper
+# EP 2019-1: A vida no Insper
 #
 # Alunos: 
 # - Aluno A: Daniel Minson Pucciariello, danielp6@al.insper.edu.br
 # - aluno B: Enrico Damiani, enricofd@al.insper.edu.br
 # - Aluno C: Giulia Castro, giuliaac@al.insper.edu.br
+
+
 
 print("Bem vindo ao jogo")
 "\n"
