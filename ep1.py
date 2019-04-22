b21bc7982f7a818a1d4dcc88dfc754e1d92d2784
Gac125 <giulia.castro12@gmail.com>
2019-04-22 18:12:53 -0300

    Quebrando a cabeça com o Enrico

--- ep1.py
+++ ep1.py
@@ -370,8 +370,10 @@ def main():
         print()
         opcoes = cenario_atual['opcoes']
         if len(opcoes) == 0:
-            print("Acabaram-se suas opções! Mwo mwo mwooooo...")
-            game_over = True
+            print("você ganhou")
+            #fazer final aqui
+#        if cenario[nome_cenario_atual][opcoes] == 1:
+#            game_over = True
         else:
             #escolha do cenario
             for e in opcoes:
